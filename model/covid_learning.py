a4e27a67feab025ec6add5f0afb653cf3cb46e18
AC0005 <changandrew777@gmail.com>
2021-02-27 16:02:48 -0800

    feature update

--- model/covid_learning.py
+++ model/covid_learning.py
@@ -1,9 +1,12 @@
 import argparse
 import logging
 import json
+import os
+import datetime
 from model.sts_model import *
 # from model.nn_model import *
 
+DATE_PATTERN="%Y-%m-%d"
 df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 dfUS = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
 dfCounties = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
@@ -78,12 +81,14 @@ def main():
     args = parser.parse_args()
 
     cumulative_cases, daily_cases, dates = get_cases(state=args.state, county=args.county)
-    start_index = dates.tolist().index(args.startdate)
-    if start_index < 0:
-        logger.error("Start date is not valid {}".format(args.startdate))
-        return -1
-    else:
+    try:
+        start_index = dates.tolist().index(args.startdate)
         print("start_date {}, index: {}".format(dates.tolist()[start_index], start_index))
+    except:
+        start_index=(datetime.datetime.strptime(args.startdate, DATE_PATTERN) \
+                    - datetime.datetime.strptime(dates[0], DATE_PATTERN)).days
+        logger.error("Start date is in future valid {}".format(args.startdate))
+
     last_index = start_index + args.days
 
     if args.model == 'sts':
@@ -95,11 +100,30 @@ def main():
         model_sts.forcast_surrogate(sample=100)
     elif args.model == "hmc":
         parameter = {}
+        last_date = datetime.datetime.strptime(dates[len(dates)-1], DATE_PATTERN).timetuple()
+        if os.path.exists((args.output)):
+            current_result=pd.read_csv(args.output)
+            last_output_date = datetime.datetime.strptime(current_result['Date'].values[len(current_result)-1], DATE_PATTERN).timetuple()
+            if last_output_date.tm_year >= last_date.tm_year and last_output_date.tm_yday > last_date.tm_yday:
+                next_output_date = (datetime.datetime.strptime(dates[len(dates)-1], DATE_PATTERN)
+                                    + datetime.timedelta(days=1)).strftime(DATE_PATTERN)
+                current_index = current_result.index[current_result['Date'] == next_output_date][0]
+                updated_daily_cases = pd.DataFrame(np.append(daily_cases, current_result[PREDICTS].loc[current_index:]))
+            else:
+                updated_daily_cases = daily_cases
+        else:
+            updated_daily_cases=daily_cases
+
+        forecast_cases = pd.DataFrame(np.zeros(last_index-start_index))
         for day in range(start_index, last_index, args.steps):
             if args.window_size > 0:
-                model_sts = modelSTS(200, daily_cases.loc[day-args.window_size:day])
+                if day > len(updated_daily_cases):
+                    cases = pd.DataFrame(np.append(updated_daily_cases.loc[day-args.window_size:],forecast_cases.loc[:day-len(daily_cases)-1]))
+                else:
+                    cases = updated_daily_cases.loc[day - args.window_size:day-1]
+                model_sts = modelSTS(200, cases)
             else:
-                model_sts = modelSTS(200, daily_cases.loc[:day])
+                model_sts = modelSTS(200, updated_daily_cases.loc[:day])
             model_sts.build_model_sts()
             model_sts.training_hmc()
             forecast_mean, forecast_scale, forecast_samples = model_sts.forcast_hmc(num_steps_forecast=args.steps)
@@ -109,8 +133,16 @@ def main():
                                   model_sts.get_actual_cases(daily_cases, day, day+args.steps),
                                   CASES_COLUMN,
                                   forecast_mean[:args.steps].numpy())
+            if day >= len(daily_cases):
+                forecast_cases.iloc[day-len(daily_cases)]=forecast_mean[:args.steps].numpy()[0]
+
             component_means, component_stddevs, forecast_component_means, forecast_component_stddevs = model_sts.get_trend_dist()
-            parameter[str(dates[day])] = {
+            if day < len(dates):
+                param_key = dates[day]
+            else:
+                param_key = (datetime.datetime.strptime(dates[0], "%Y-%m-%d")+datetime.timedelta(days=day)).strftime("%Y-%m-%d")
+
+            parameter[str(param_key)] = {
                 "component_means": component_means,
                 "component_stddevs": component_stddevs,
                 "forecast_component_means": forecast_component_means,
